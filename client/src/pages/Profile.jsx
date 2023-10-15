6c34bd0209b776a21626b1f6d23079c02898028c
Deepak Rizal <deepak2000rizal@gmail.com>
2023-10-15 14:49:12 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -281,7 +281,9 @@ function Profile() {
                 >
                   Delete
                 </button>
-                <button className=" text-green-700 uppercase">EDIT</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className=" text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
