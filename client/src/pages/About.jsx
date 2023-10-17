a0ef8ef7f29535275bfd06bee3a3b5c02e57b6e0
Deepak Rizal <deepak2000rizal@gmail.com>
2023-10-17 17:59:29 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,28 @@
-function About() {
-  return <div>About</div>;
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Comp Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Comp Estate is a leading real estate agency that specializes in helping
+        clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptas
+        perferendis ipsa libero. Molestias quibusdam amet tempora magnam
+        explicabo dicta vitae dignissimos maxime atque saepe soluta, illo
+        reprehenderit. Voluptas beatae nemo non blanditiis ipsa.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, iste
+        praesentium! Mollitia eligendi corporis ipsum amet minus totam, sunt
+        aliquid nihil vel ab qui doloremque perferendis vitae quisquam inventore
+        sit quidem officiis saepe quos?
+      </p>
+    </div>
+  );
 }
-
-export default About;
